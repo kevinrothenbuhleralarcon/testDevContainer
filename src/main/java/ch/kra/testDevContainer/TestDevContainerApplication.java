4172784bb74fd8f3c4ada1d8611a2e349400492f
kevin.rothenbuhler-alarcon <kevin.rothenbuhler-alarcon@softcom.pro>
2026-02-17 17:13:00 +0100

    Initial commit

---
+++ src/main/java/ch/kra/testDevContainer/TestDevContainerApplication.java
@@ -0,0 +1,13 @@
+package ch.kra.testDevContainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDevContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDevContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/kra/testDevContainer/TestDevContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.kra.testDevContainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDevContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
